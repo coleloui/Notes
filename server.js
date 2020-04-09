053b41a7798b841353f4a09df3f8ecddb0612190
Louis Coleman <coleloui18@gmail.com>
2020-04-09 09:44:27 -0700

    start script

--- server.js
+++ server.js
@@ -29,14 +29,18 @@ app.post("/api/notes", function (req, res) {
   const lastElement = db.length > 0 ? db[db.length - 1] : null;
   const id = lastElement !== null ? lastElement.id + 1 : 1;
   const newDb = [...db, { ...newNote, id }];
-  fs.writeFileSync(
-    "./db/db.json",
-    JSON.stringify(newDb),
-    "utf8"
-  );
+  fs.writeFileSync("./db/db.json", JSON.stringify(newDb), "utf8");
   res.json(newDb);
 });
 
+app.delete("/api/notes/:id", function (req, res) {
+  const db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  const id = req.params.id;
+  console.log(db);
+  const newDb = db.filter((note) => note.id !== Number(id));
+  fs.writeFileSync("./db/db.json", JSON.stringify(newDb), "utf8");
+  res.json(newDb);
+});
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
